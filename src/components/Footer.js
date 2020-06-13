49de288f3a40ef44aacc153df3589d2c7c030ef7
darius khayyam <dv.Khayyam@gmail.com>
2020-06-13 20:34:33 +0200

    ok this is the real commit

---
+++ src/components/Footer.js
@@ -0,0 +1,9 @@
+import React from "react"
+
+function Footer(){
+    return(
+        <h1>GOODBYE!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!</h1>
+    )
+}
+
+export default Footer
---
+++ src/components/MainContent.js
@@ -0,0 +1,15 @@
+import React from "react"
+
+function MainContent(){
+    return(
+        <par>
+            <ul>
+                <li>This is a good website</li>
+                <li>I am the best programmer</li>
+                <li>I love you shali 😗</li>
+            </ul>
+        </par>
+    )
+}
+
+export default MainContent
